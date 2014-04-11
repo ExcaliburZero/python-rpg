68b2185d53ead46632b6a08b186b80ced16fc7da
rocketslime11 <cwellsny@nycap.rr.com>
2014-04-10 21:32:23 -0400

    added Training Grounds and modified Forest Training code
    added "Training Grounds.py" and "Times/Training Grounds.txt", and
    changed "Forest Training.py" so it has a get_character_info() function

--- Training Grounds.py
+++ Training Grounds.py
@@ -1,5 +1,5 @@
 """
-Forest Training
+Training Grounds
 """
 
 #Imports
@@ -39,11 +39,20 @@ def get_character_info():
     resistance = int(info['Resistance'])
     luck = int(info['Luck'])
 
+#Define function to modify stat
+def stat_change(stat, mod):
+    ifs = ""
+    if not mod == 1:
+        ifs = "s"
+    stat_lowercase = stat.lower()
+    info[str(stat)] = str(eval(stat_lowercase) + mod)
+    print(stat + " goes up by " + str(mod) + " point" + ifs + "!")
+
 #Set current time
 now = datetime.now()
 
 #Open time file for reading
-time_file = open("Times/Forest Training.txt", "r")
+time_file = open("Times/Training Grounds.txt", "r")
 
 #Setting up time variables
 last_time = int(time_file.read())
@@ -57,11 +66,11 @@ print("It is now: " + str(now.minute))
 time_file.close()
 
 #Test if five minutes have occured since last play
-if now_time - last_time >= 5 or now_time - last_time < 0:
+if now_time - last_time >= 3 or now_time - last_time < 0:
     print("Ready!")
 else:
     #if less than five minutes have elapsed then gives message and ends program
-    print("It has been " + str(now_time - last_time) + " minutes since you last played. Wait until it has been at least 5 minutes.")
+    print("It has been " + str(now_time - last_time) + " minutes since you last played. Wait until it has been at least 3 minutes.")
     sys.exit()
     
 get_character_info()
@@ -70,24 +79,42 @@ get_character_info()
 seed = randint(1,100)
 print(str(seed))
 
-#Possibility 1 ~ 0-33 ~ Increase Health 2
-if seed <= 33:
-    info['Health'] = str(health + 2)
-    print("Health goes up two points!")
-
-#Possibility 2 ~ 34-65 ~ Increase Health 1 and Strength 1
-if seed > 33 and seed < 66:
-    info['Health'] = str(health + 1)
-    info['Strength'] = str(strength + 1)
-    print("Health and Strength both go up one point!")
-
-#Possibility 3 ~ 66-100 ~ Increase Strength 2
-if seed >= 66:
-    info['Strength'] = str(strength + 2)
-    print("Strength goes up two points!")
+#Possibility 1 ~ 0-11 ~ Increase Health 1
+if seed <= 11:
+    stat_change("Health", 1)
+    
+#Possibility 1 ~ 12-22 ~ Increase Strength 1
+if seed <= 22 and seed > 11:
+    stat_change("Strength", 1)
+    
+#Possibility 1 ~ 23-33 ~ Increase Defense 1
+if seed <= 33 and seed > 22:
+    stat_change("Defense", 1)
+    
+#Possibility 1 ~ 34-44 ~ Increase Magic 1
+if seed <= 44 and seed > 33:
+    stat_change("Magic", 1)
+    
+#Possibility 1 ~ 45-55 ~ Increase Resistance 1
+if seed <= 55 and seed > 44:
+    stat_change("Resistance", 1)
+    
+#Possibility 1 ~ 56-66 ~ Increase Luck 1
+if seed <= 66 and seed > 55:
+    stat_change("Luck", 1)
+    
+#Possibility 1 ~ 67-77 ~ Increase Gold 50
+if seed <= 77 and seed > 66:
+    info['Gold'] = str(gold + 50)
+    print("You find 50 Gold!")
+    
+#Possibility 1 ~ 78-100 ~ Increase Gold 20
+if seed <= 100 and seed > 77:
+    info['Gold'] = str(gold + 20)
+    print("You find 20 Gold!")
 
 #Open time_file for writing
-time_file = open("Times/Forest Training.txt", "w")
+time_file = open("Times/Training Grounds.txt", "w")
 
 #Update time
 time_write = str(now.minute)
